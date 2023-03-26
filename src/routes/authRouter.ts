310bb283bccd59c76983dc797fc4ef8d3a4b0337
Carlos <carlos@pop-os.localdomain>
2023-03-25 23:42:25 -0300

    fix: change get request from signin route to a post request

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -7,6 +7,6 @@ import { authSchema } from "../schemas/authSchema.js";
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchemaMiddleware(authSchema), signup);
-authRouter.get("/signin", validateSchemaMiddleware(authSchema), signin);
+authRouter.post("/signin", validateSchemaMiddleware(authSchema), signin);
 
 export default authRouter;
